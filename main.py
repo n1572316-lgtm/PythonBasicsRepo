9f7a6bb3a1528420a998d035feeeece0066ad2bf
n1572316-lgtm <n1572316@gmail.com>
2025-10-28 18:21:55 +0300

    Adding paper_game.py

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
-num: int = int(input())
+# num: int = int(input())
 
-nums: list[int] = []
+# nums: list[int] = []
 
-for i in range(num):
-    nums.append(int(input()))
-
-
-    index: int = 0
-
-    while index < len(nums):
-        if nums[i] % 3 == 0 or nums[i] % 5 == 0:
-            nums.pop(i)
-        index += 1
-
-    for num in nums:
-        print(num, end="")
+# for i in range(num):
+#     nums.append(int(input()))
 
 
-print(nums)
+#     index: int = 0
+
+#     while index < len(nums):
+#         if nums[i] % 3 == 0 or nums[i] % 5 == 0:
+#             nums.pop(i)
+#         index += 1
+
+#     for num in nums:
+#         print(num, end="")
+
+
+# print(nums)
+
+
+# u_c: str = input("Введите фигуру")
+# с_с: str = 
+
+matrix: list[list[int]] = []
+
+for i in range(1, 6):
+    row: list[int] =[]
+    for j in range(1, 6):
+        row.append(i*5+j+1)
+    matrix.append(row)
+
+for row in matrix:
+    print(row) 
+
+diag_sum: int = 0
+
+for i in range(len(matrix)):
+    diag_sum += matrix[i][i]
+
+print(diag_sum)
+
+diag_sum_list: int = sum([matrix[i][i]] for i in range(len(matrix)))
+print(diag_sum_list)
+print()
---
+++ paper_game.py
@@ -0,0 +1,27 @@
+# import random
+
+# figures: list[str] = ["камень", "ножницы", "бумага"]
+                      
+# while True:
+#     u_c: str = input("Введите фигуру: ")
+#     c_c: str = random.choice(figures)
+
+#     print("Выбор компа:", c_c)
+
+#     if (u_c =="камень" and c_c =="ножницы")\
+#         or (u_c =="ножницы" and c_c == "бумага")\
+#         or (u_c == "бумага" and c_c == "камень"):
+#         print("WIN!!!") 
+#     elif u_c == c_c:
+#         print("TIE!!!")
+#     elif u_c not in figures:
+#         print("Figure missmatch!!!")
+#     else:
+#         print("LOOSE!!!")
+
+#     user_break: str = input("Du u wanna continue? (y/n):")
+
+#     if user_break == "n":
+#         break
+#     elif user_break not in ["y", "n"]:
+#          print("I don't understand!") 
